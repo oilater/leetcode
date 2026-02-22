9c05185ff5aeb7d582345c858559c877f3071186
김성현 <115062965+oilater@users.noreply.github.com>
2026-02-23 02:23:36 +0900

    Time: 2 ms (72.36%), Space: 59.3 MB (44.53%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.ts
@@ -0,0 +1,16 @@
+function majorityElement(nums: number[]): number {
+    let candidate = 0;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        if (num === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return candidate;
+};
