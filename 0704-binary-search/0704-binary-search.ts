7c01af5a01dca261b865c60b03397d89e35a9a8e
김성현 <115062965+oilater@users.noreply.github.com>
2026-02-22 18:57:43 +0900

    Time: 0 ms (100%), Space: 58.6 MB (69.27%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.ts
@@ -0,0 +1,23 @@
+
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((right - left) / 2) + left;
+
+        if (target > nums[mid]) {
+            left = mid + 1;
+            
+        }
+        
+        if (target < nums[mid]) {
+            right = mid - 1;
+        }
+
+        if (target === nums[mid]) {
+            return mid;
+        }
+    }
+    return -1;
+};
