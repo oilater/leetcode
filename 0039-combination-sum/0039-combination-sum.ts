0e9fd6720c4266da48b03bd13639cb80494aea34
김성현 <115062965+oilater@users.noreply.github.com>
2026-04-05 20:00:26 +0900

    Time: 1 ms (96.38%), Space: 63.9 MB (20.97%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.ts
@@ -0,0 +1,39 @@
+/**
+[2, 3, 5], target: 8
+- 일단 배열 정렬하기
+
+더할 때는 res에 푸시하기 위해 배열에 차근차근 저장해놔야 한다
+
+각 인덱스의 숫자를 뽑고나서 할 수 있는 것 (반복해야 할 것)
+- 자기 자신을 더하거나
+- 다음 인덱스의 숫자를 더하기
+
+-> 더한 총합(reduce로 구하기)이 target보다 크다면 종료
+-> 더한 숫자가 target과 같다면 temp에 쌓고니서 push
+*/
+
+function combinationSum(candidates: number[], target: number): number[][] {
+    const res = [];
+
+    function recursion(temp: number[], curIndex: number) {
+        const temps = [...temp];
+        const sum = temps.reduce((acc, cur) => acc + cur, 0);
+        if (sum === target) {
+            res.push(temps);
+            return;
+        } else if (sum > target) {
+            return;
+        }
+        
+        // 다음 숫자 뽑기 (이전에 지난 숫자는 안뽑아도 됨)
+        for (let nextIndex = curIndex; nextIndex < candidates.length; nextIndex++) {
+            // temp에 숫자 넣어보고
+            temps.push(candidates[nextIndex]);
+            recursion(temps, nextIndex);
+            // 리턴 됐다면 숫자 뺴기
+            temps.pop();
+        }
+    }
+    recursion([], 0);
+    return res;
+};
