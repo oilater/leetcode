acf930792843e31a0f06bdf1a33f8144b5a7ae67
김성현 <115062965+oilater@users.noreply.github.com>
2026-04-12 20:17:55 +0900

    Time: 0 ms (100%), Space: 57.1 MB (66.33%) - LeetHub

---
+++ 0078-subsets/0078-subsets.ts
@@ -0,0 +1,26 @@
+/**
+골랐으면 건너뛰어야 함
+각 턴에서 할 수 있는 행동
+- 뽑거나
+- 안 뽑거나
+
+현재 인덱스 뽑거나 넘기거나
+
+*/
+function subsets(nums: number[]): number[][] {
+    if (!nums.length) return [[]];
+    const result = [];
+    
+    function comb(depth: number, path: number[]) {
+        result.push([...path]);
+        if (depth === nums.length) return;
+        
+        for (let i = depth; i < nums.length; i++) {
+            path.push(nums[i]);
+            comb(i + 1, path);
+            path.pop();
+        }
+    }
+    comb(0, []);
+    return result;
+};
