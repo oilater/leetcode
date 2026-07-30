c7e8381b47891bf22ab54fdb207c016250d73193
Seonghyeon Kim <115062965+oilater@users.noreply.github.com>
2026-07-30 11:54:56 +0900

    Time: 44 ms (87.35%), Space: 58.3 MB (58.7%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     neighbors: _Node[]
+ * 
+ *     constructor(val?: number, neighbors?: _Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.neighbors = (neighbors===undefined ? [] : neighbors)
+ *     }
+ * }
+ * 
+ */
+function cloneGraph(node: _Node | null): _Node | null {
+	if (!node) return null;
+
+  const cloned = new Map<_Node, _Node>();
+
+  function copy(origin: _Node) {
+    if (cloned.has(origin)) return cloned.get(origin);
+
+    const dup = new _Node(origin.val);
+    cloned.set(origin, dup);
+
+    for (const neighbor of origin.neighbors) {
+      dup.neighbors.push(copy(neighbor));
+    }
+    return dup;
+  }
+  return copy(node);
+};
