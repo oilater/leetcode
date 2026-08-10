2e9c0905b777aa40da86b116f9f5d1dcddfa07a0
Seonghyeon Kim <115062965+oilater@users.noreply.github.com>
2026-08-11 00:30:21 +0900

    Time: 0 ms (100%), Space: 57.7 MB (28.18%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    if (root === null) return [];
+    const answer: number[] = [];
+    const queue: TreeNode[] = [root];
+
+    while (queue.length) {
+        const levelSize = queue.length;
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            if (i === levelSize - 1) {
+                answer.push(node.val);
+            }
+
+            if (node.left !== null) {
+                queue.push(node.left);
+            }
+
+            if (node.right !== null) {
+                queue.push(node.right);
+            } 
+        }
+    }
+    return answer;  
+};
